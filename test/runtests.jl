d38bff136731ec1cbc58b4ca627e02a4a70ab016
EdgyEggplant <steve_xu1995@yahoo.com.au>
2016-10-20 10:57:59 +1100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,8 @@ f(x)=mean(x)>=0.85 ? 1. : 0.
 srand(5)
 sim=ais(f,3,t0=[1;0;1],accel="saa")
 
-@test_approx_eq sim.μ 0.015348602479358703
+@test_approx_eq sim.μ[end] 0.015348602479358703
 
-@test_approx_eq sim.θ [1.37287, 0.436164, 1.37691]
+@test_approx_eq sim.θ[end,:] [1.37287, 0.436164, 1.37691]
 
-@test_apprxo_eq sim.λ [1.5, 0.5, 1.5]
+@test_apprxo_eq sim.λ [1.5; 0.5; 1.5]
