145b86b11caab8caafe8fc90602665d6caf5b96f
EdgyEggplant <steve_xu1995@yahoo.com.au>
2016-10-19 14:30:19 +1100

    Update AdaptiveIS.jl

--- src/AdaptiveIS.jl
+++ src/AdaptiveIS.jl
@@ -1,7 +1,7 @@
-__precompile__()
 module AdaptiveIS
 
-using Distributions
+using Distributions,Plots
+export g,ginv,h,gh,r,gn,maxd,points,maxl,t,gq,saa,ais_type,Base.show,ais,Plots.plot,Plots.plot!
 
 g(z::Vector{Float64},t::Vector{Float64},t0::Vector{Float64})=cdf(Normal(),z-t).*(1.-t0)+exp(-t.*z).*t0
 
