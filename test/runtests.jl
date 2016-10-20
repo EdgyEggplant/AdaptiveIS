a2f538fc819d891f282c2bbb19ff0f470eff3e32
EdgyEggplant <steve_xu1995@yahoo.com.au>
2016-10-20 19:13:12 +1100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,3 @@ srand(5)
 sim=ais(f,3,t0=[1;0;1],accel="saa")
 
 @test_approx_eq_eps sim.μ[end] 0.0151874 1e-3
-
-@test_approx_eq_eps sim.θ[end,:] [1.37287, 0.436164, 1.37691] 1e-2
-
-@test_approx_eq sim.λ [1.5; 0.5; 1.5]
