e9d2f202c1f23396af191b0c49f8dfa57f033f98
EdgyEggplant <steve_xu1995@yahoo.com.au>
2016-10-20 11:02:51 +1100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,6 @@ sim=ais(f,3,t0=[1;0;1],accel="saa")
 
 @test_approx_eq sim.μ[end] 0.015348602479358703
 
-@test_approx_eq sim.θ[end,:] [1.37287, 0.436164, 1.37691]
+@test_approx_eq_eps sim.θ[end,:] [1.37287, 0.436164, 1.37691] 1e-4
 
 @test_apprxo_eq sim.λ [1.5; 0.5; 1.5]
