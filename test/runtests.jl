cd15c2af173f8c7bf26b9ed032c08f92ae1873e2
EdgyEggplant <steve_xu1995@yahoo.com.au>
2016-10-21 21:27:11 +1100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,15 @@
 using AdaptiveIS, Base.Test
 
 f(x)=mean(x)>=0.85 ? 1. : 0.
-srand(5)
-sim=ais(f,3,t0=[1;0;1],accel="saa")
 
+srand(5)
+sim=ais(f,3)
+@test_approx_eq_eps sim.μ[end] 0.0151874 1e-3
+
+srand(5)
+sim=ais(f,3,t0=[1;0;1])
+@test_approx_eq_eps sim.μ[end] 0.0151874 1e-3
+
+srand(5)
+sim=ais(f,3,t0=[1;1;0],accel="saa")
 @test_approx_eq_eps sim.μ[end] 0.0151874 1e-3
