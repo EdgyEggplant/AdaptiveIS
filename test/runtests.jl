bc43152b9628f68b62c32f74e4bc3e3aa9827dad
EdgyEggplant <steve_xu1995@yahoo.com.au>
2016-10-23 22:10:11 +1100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,3 +13,15 @@ sim=ais(f,3,t0=[1;0;1])
 srand(5)
 sim=ais(f,3,t0=[1;1;0],accel="saa")
 @test_approx_eq_eps sim.μ[end] 0.0151874 1e-3
+
+srand(5)
+sim=ais(f,3,dimreduc=true)
+@test_approx_eq_eps sim.μ[end] 0.0151874 1e-3
+
+srand(5)
+sim=ais(f,3,t0=1.)
+@test_approx_eq_eps sim.μ[end] 0.0151874 1e-3
+
+srand(5)
+sim=ais(f,3,t0=1.,accel="saa")
+@test_approx_eq_eps sim.μ[end] 0.0151874 1e-3
